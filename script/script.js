7af7f6d177ed3e6a2d8fc5702fa4d8ba9e9ebe64
Minhal Abbas <minhalabbas183@gmail.com>
2023-11-06 18:06:09 +0500

    png turned white

--- script/script.js
+++ script/script.js
@@ -4,12 +4,20 @@ const travel = document.querySelector(".travel");
 const form = document.querySelector("#form");
 const form_switch = document.querySelectorAll(".form_switch");
 
+var Image1 = document.querySelector(".btnImg1");
+var Image2 = document.querySelector(".btnImg2");
+var Image3= document.querySelector(".btnImg3");
+
 
 function showAlert(){
   alert ("Hello world!");
 }
 
 function tab1(){
+    Image2.classList.remove("active");
+    Image3.classList.remove("active");
+    Image1.classList.add("active");
+
     form.style.marginLeft = "0";
     health.style.background = "none" 
     travel.style.background = "none";
@@ -21,6 +29,10 @@ function tab1(){
 }
 
 function tab2(){
+    Image1.classList.remove("active");
+    Image3.classList.remove("active");
+    Image2.classList.add("active");
+
     form.style.marginLeft = "-100%";
     home.style.background = "none";
     health.style.background = "none";
@@ -32,6 +44,10 @@ function tab2(){
 }
 
 function tab3(){
+    Image2.classList.remove("active");
+    Image1.classList.remove("active");
+    Image3.classList.add("active");
+
     form.style.marginLeft = "-200%";
     home.style.background = "none";
     travel.style.background = "none";
